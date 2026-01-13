529749aaf48de83a2e3d64ffe08e7a35b02248de
aad-law <aadeshlawate2@outlook.com>
2026-01-13 23:39:14 +0530

    Clean project structure and ignore Eclipse workspace files

--- First_Module/src/Abstraction/Animal.java
+++
@@ -1,9 +0,0 @@
-package Abstraction;
-
-public abstract  class Animal {
-	
-	public abstract void eat();
-	 public abstract void sleep();
-	 public abstract void sex();
-	
-}
--- First_Module/src/Interface/Animal.java
+++
@@ -1,7 +0,0 @@
-package Interface;
-
-public interface Animal {
-
-	public  static  int age =22;
-	
-}
