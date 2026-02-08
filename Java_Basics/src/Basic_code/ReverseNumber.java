687d6aaa3e6f5be7ced8af095587bf3d0f6adaca
aad-law <aadeshlawate2@outlook.com>
2026-02-08 19:57:37 +0530

    update

---
+++ Java_Basics/src/Basic_code/ReverseNumber.java
@@ -0,0 +1,21 @@
+package Basic_code;
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Num plz:");
+
+        int num = sc.nextInt();
+        int res = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            res = res * 10 + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Reverse = " + res);
+    }
+}
