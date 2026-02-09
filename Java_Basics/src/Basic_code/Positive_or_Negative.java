5d93d6e2bed9fcf528260a3c0d727240bcadd245
aad-law <aadeshlawate2@outlook.com>
2026-02-09 18:56:12 +0530

    Positive or Negative

---
+++ Java_Basics/src/Basic_code/Positive_or_Negative.java
@@ -0,0 +1,22 @@
+package Basic_code;
+import java.util.Scanner;
+
+
+
+public class Positive_or_Negative {
+	
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	 int num = sc.nextInt();
+	 
+	 if(num>0) {
+		 System.out.println("It is positive");
+	 }else {
+		 System.out.println("IT is negative");
+	 }
+}
+	
+	
+
+}
